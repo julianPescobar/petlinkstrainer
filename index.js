3a1a3fb98d6d8f49145848b03eb8fc9e193f135b
Escobar Julián <magneticrex@gmail.com>
2020-07-14 18:05:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 
 app.get('/', function(req, res) {
-    app.use(express.static(path.join(__dirname + '/public/pets/')));
+    app.use(express.static(path.join(__dirname + '/')));
     res.sendFile(path.join(__dirname + '/match-game.html'));
 });
 console.log("app listening on 8080");
